c32270d4ed448370b4d20f3a7e6150b9c9956e27
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-09-13 17:25:22 +0900

    20231913 프머공부

--- pg_git/Lv1_watermelon.java
+++ pg_git/Lv1_watermelon.java
@@ -2,9 +2,10 @@ package pg_git;
 
 public class Lv1_watermelon {
 	public String solution(int n) {
-		char[] wm = new char[n/2+1];
-		return String.valueOf(wm).replace("\0", "수박").substring(0, n);
+		char[] wm = new char[n/2+1]; // 홀수와 짝수 모두 사용할 수 도있도록 1을 더해서 사용
+		return String.valueOf(wm).replace("\0", "수박").substring(0, n); //substring으로 정리
 	}
+	
 //	public String solution(int n) {
 //		StringBuilder sb = new StringBuilder();
 //		if (n % 2 == 0) {
