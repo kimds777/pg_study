fdf12438a72aa73d990fe2a4a88d3af28f326373
kimds777 <77198242+kimds777@users.noreply.github.com>
2024-03-25 22:24:41 +0900

    Fix : 20240325 프로그래머스 Lv2. 구명보트

--- pg_git/Lv2_rescueBoat.java
+++ pg_git/Lv2_rescueBoat.java
@@ -1,38 +1,31 @@
 package pg_git;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
-class RescueBoat { // 효율성 테스트 시간초과..! 수정필요!
+class RescueBoat { // Deque 활용!!
 	public int solution(int[] people, int limit) {
 
-		int numberOfRescueBoat = 0;
-		Arrays.sort(people);
+		Arrays.sort(people); // 무인도에 갇힌 사람들 몸무게 배열을 오름차순으로 정렬
 
-		for (int i = people.length - 1; i >= 0; i--) {
-			if (people[i] == 0) {
-				break;
+		Deque<Integer> pp = new ArrayDeque<>(); // 가장 무거운 사람과 가장 가벼운 사람을 가져올 수 있도록 Deque 사용
+		for (int p : people) { // 오름차순으로 정렬된 몸무게를 Deque에 넣는다.
+			pp.add(p);
+		}
+
+		int numberOfRescueBoat = 0; // 필요한 구명보트 개수
+
+		while (!pp.isEmpty()) { // Deque가 비어있는 않는 경우
+			int weight = pp.pollLast(); // 가장 무거운 몸무게를 Deque에서 추출?하고 삭제
+
+			// Deque에 몸무게가 1개 이상있고 
+			// 가장 무거운 몸무게 + 가장 가벼운 몸무게가 무게 제한보다 같거나 작은 경우
+			if (pp.size() >= 1 && weight + pp.peekFirst() <= limit) { 
+				pp.pollFirst(); // 가장 가벼운 몸무게를 삭제
 			}
 
-			if (i == 0) {
-				System.out.println("check-1");
-				numberOfRescueBoat++;
-				break;
-			}
-
-			for (int j = 0; j < people.length; j++) {
-				if (people[j] == 0) {
-					continue;
-				}
-
-				if (people[i] + people[j] <= limit) {
-					System.out.println("check-2 : " + people[i] + ", " + people[j]);
-					people[j] = 0;
-					break;
-				}
-			}
-
-			people[i] = 0;
-			numberOfRescueBoat++;
+			numberOfRescueBoat++; // 구명보트 개수를 1씩 증가시킨다
 		}
 
 		return numberOfRescueBoat;
