2014c5d572f72073a7a2be10c225a3ffef7494f6
kimds777 <77198242+kimds777@users.noreply.github.com>
2024-10-05 15:52:07 +0900

    Feat : 20241005 Lv0.문자 출력

---
+++ pg_git/Lv0_stringOutput.java
@@ -0,0 +1,10 @@
+package pg_git;
+
+public class Lv0_stringOutput {
+	public static void main(String[] args) {
+		String message = "Let's go!";
+
+		System.out.println("3\n2\n1");
+		System.out.println(message);
+	}
+}
