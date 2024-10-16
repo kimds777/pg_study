58f3c964f4c01f9c6ff70b2b2fbf1f3a692d6dd0
Daseul Kim <tina27849@gmail.com>
2024-10-16 10:55:46 +0900

    Fix : 20241016 Lv2.귤고르기

--- pg_git/Lv2_choosingTangerines.java
+++ pg_git/Lv2_choosingTangerines.java
@@ -1,24 +1,43 @@
 package pg_git;
 
-import java.util.HashMap;
+import java.util.*;
 
 class ChoosingTangerines {
     public static int solution(int k, int[] tangerine) {
         HashMap<Integer, Integer> tangerineMap = new HashMap<>();
 
-        for(int t : tangerine){
-            if(!tangerineMap.containsKey(t)){
-                tangerineMap.put(t,0);
+        for (int t : tangerine) {
+            if (!tangerineMap.containsKey(t)) {
+                tangerineMap.put(t, 0);
             }
 
-            tangerineMap.put(t, tangerineMap.get(t)+1);
+            tangerineMap.put(t, tangerineMap.get(t) + 1);
         }
 
+        List<Integer> keySet = new ArrayList<>(tangerineMap.keySet());
+        keySet.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return tangerineMap.get(o2).compareTo(tangerineMap.get(o1));
+            }
+        });
 
         int answer = 0;
+
+        for(int key : keySet){
+
+            k -= tangerineMap.get(key);
+            answer++;
+
+            if(k <= 0){
+                break;
+            }
+        }
+
         return answer;
     }
 }
+
 public class Lv2_choosingTangerines {
     public static void main(String[] args) {
         int[] tangerines = {1, 3, 2, 5, 4, 5, 2, 3};
